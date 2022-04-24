14844efd0a61ac08b1640d5ceae4deabc538aa2d
Nobuhiro Tabuki <japanese.around30@gmail.com>
2022-04-24 21:10:25 +0900

    samples

--- src/DBFixtureJs.ts
+++ src/DBFixtureJs.ts
@@ -1,9 +1,27 @@
+import Excel from 'exceljs'
+
 export class DBFixtureJs {
   constructor(option: {}) {}
 
-  public load() {
+  public async load(filepath: string = './examples/testdata1.xlsx'): Promise<void> {
     console.log('load()')
+    const workbook = new Excel.Workbook()
+    const worksheet = await workbook.xlsx.readFile(filepath)
+    // console.dir(worksheet)
+    let worksheet1 = worksheet.getWorksheet(1)
+    console.log('シート名: ' + worksheet1.name)
+    worksheet1.eachRow((row, rowNumber) => {
+      console.log(`row = ${rowNumber}`)
+      row.eachCell((cell, colNum) => {
+        console.log(`cell.type =  ${cell.type}, value = ${cell.value}`)
+        for (let v in Excel.ValueType) {
+          console.log('  v = ', v)
+        }
+      })
+    })
+    return
   }
+
   public export() {
     console.log('export()')
   }
