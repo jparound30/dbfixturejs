0fb620c5cb3ae99f793dba2ac048bd50813f50dd
Nobuhiro Tabuki <japanese.around30@gmail.com>
2022-06-16 23:08:10 +0900

    add ddl

--- dbfixturejs_docker/initdb/00_create_database.sql
+++ dbfixturejs_docker/initdb/00_create_database.sql
@@ -1,5 +1,2 @@
 DROP DATABASE IF EXISTS test;
 CREATE DATABASE test;
-
-GRANT ALL ON test.* TO 'admin'@'%';
-
---
+++ dbfixturejs_docker/initdb/02_create_table.sql
@@ -0,0 +1,13 @@
+use test;
+
+-- 数値型主要なもの
+DROP TABLE IF EXISTS number_cols;
+CREATE TABLE number_cols (
+  k integer auto_increment PRIMARY KEY,
+  c_bit BIT,
+  c_int INT,
+  c_bigint BIGINT,
+  c_decimal DECIMAL(10,5),
+  c_double DOUBLE
+);
+
