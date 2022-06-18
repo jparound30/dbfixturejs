6dd345993bdeabc026afb8ea41e3ea553f996b6e
Nobuhiro Tabuki <japanese.around30@gmail.com>
2022-06-19 00:00:06 +0900

    mod ddl

--- dbfixturejs_docker/initdb/01_create_user.sql
+++ dbfixturejs_docker/initdb/01_create_user.sql
@@ -1,3 +1,3 @@
 DROP USER IF EXISTS 'user';
 CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'user12345!';
-GRANT ALL ON test.* TO 'user'@'%';
+GRANT ALL ON *.* TO 'user'@'%' WITH GRANT OPTION;
