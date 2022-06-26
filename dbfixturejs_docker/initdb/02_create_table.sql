29a88e43d65d596ccbea1f2e62c196236a738f75
Nobuhiro Tabuki <japanese.around30@gmail.com>
2022-06-26 09:40:36 +0900

    test: 日付型に関連するテーブル追加

--- dbfixturejs_docker/initdb/02_create_table.sql
+++ dbfixturejs_docker/initdb/02_create_table.sql
@@ -2,12 +2,25 @@ use test;
 
 -- 数値型主要なもの
 DROP TABLE IF EXISTS number_cols;
-CREATE TABLE number_cols (
-  k integer auto_increment PRIMARY KEY,
-  c_bit BIT(10),
-  c_int INT,
-  c_bigint BIGINT,
-  c_decimal DECIMAL(10,5),
-  c_double DOUBLE
+CREATE TABLE number_cols
+(
+    k         integer auto_increment PRIMARY KEY,
+    c_bit     BIT(10),
+    c_int     INT,
+    c_bigint  BIGINT,
+    c_decimal DECIMAL(10, 5),
+    c_double  DOUBLE
 );
 
+
+-- 日時型主要なもの
+DROP TABLE IF EXISTS datetime_cols;
+CREATE TABLE datetime_cols
+(
+    k           integer auto_increment PRIMARY KEY,
+    c_datetime  DATETIME,
+    c_timestamp TIMESTAMP,
+    c_date      DATE,
+    c_time      TIME,
+    c_year      YEAR
+)
