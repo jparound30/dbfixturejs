0f1da1bc47bab8050d25d2d4a83df3e7dbbe7fef
Nobuhiro Tabuki <japanese.around30@gmail.com>
2022-06-27 22:22:30 +0900

    refactor: divide classes, types

--- src/DBFixtureJs.ts
+++ src/DBFixtureJs.ts
@@ -1,7 +1,7 @@
 import ExcelJS from 'exceljs'
 import mysql2, { ResultSetHeader } from 'mysql2/promise'
-import dayjs from 'dayjs'
 import { DBFixtureJsOptions } from './index'
+import { ColumnTypes, RowData, TableData } from './TableData'
 
 export interface DBFixtureConnOpts {
   host: string
@@ -172,7 +172,7 @@ function excelValueTypeToString(type: number): string | undefined {
   return convTblForExcelValueType.get(type)
 }
 
-const DBColumnTypes = {
+export const DBColumnTypes = {
   DECIMAL: 0,
   TINY: 1,
   SHORT: 2,
@@ -216,11 +216,9 @@ function typeToString(type: number): string | undefined {
   return convTbl.get(type)
 }
 
-type RowData = Array<string | number | Date | null>
+export type TypesVal = typeof DBColumnTypes[keyof typeof DBColumnTypes]
 
-type TypesVal = typeof DBColumnTypes[keyof typeof DBColumnTypes]
-
-const isStringColumnType = (v: TypesVal): boolean => {
+export const isStringColumnType = (v: TypesVal): boolean => {
   return (
     v === DBColumnTypes.VARCHAR ||
     v === DBColumnTypes.ENUM ||
@@ -232,63 +230,4 @@ const isStringColumnType = (v: TypesVal): boolean => {
     v === DBColumnTypes.VAR_STRING ||
     v === DBColumnTypes.STRING
   )
-}
-
-type ColumnTypes = Array<{ columnName: string; columnType: TypesVal }>
-
-class TableData {
-  readonly schemaName: string | null
-  readonly name: string
-  data: RowData[]
-  columnTypes: ColumnTypes
-  readonly emptyStr: string
-
-  get tableName() {
-    return this.schemaName ? `${this.schemaName}.${this.name}` : `${this.name}`
-  }
-
-  constructor(init: { schemaName?: string; name: string; data?: RowData[]; columnTypes: ColumnTypes; emptyStr: string }) {
-    this.schemaName = init.schemaName ?? null
-    this.name = init.name
-    this.data = init.data ?? []
-    this.columnTypes = init.columnTypes
-    this.emptyStr = init.emptyStr
-  }
-
-  public createInsertSql(): string {
-    const sql1 = `INSERT INTO ${this.tableName} `
-    const cols = '(' + this.columnTypes.map((v) => v.columnName).join(',') + ') VALUES \n'
-    const values = this.data.map((row) => {
-      const rowStr = row
-        .map((col, index) => {
-          const columnType = this.columnTypes[index].columnType
-          if (isStringColumnType(this.columnTypes[index].columnType)) {
-            if (col !== null && col.toString().match(this.emptyStr)) {
-              return mysql2.escape('')
-            }
-          }
-          if (columnType === DBColumnTypes.BIT) {
-            return `b'${col}'`
-          } else if (columnType === DBColumnTypes.DATETIME) {
-            const val = dayjs(col).format('YYYY-MM-DD HH:mm:ss')
-            return mysql2.escape(val)
-          } else if (columnType === DBColumnTypes.TIMESTAMP) {
-            const val = dayjs(col).format('YYYY-MM-DD HH:mm:ss')
-            return mysql2.escape(val)
-          } else if (columnType === DBColumnTypes.DATE) {
-            const val = dayjs(col).format('YYYY-MM-DD')
-            return mysql2.escape(val)
-          }
-          if (typeof col === 'string') {
-            return mysql2.escape(col)
-          } else if (typeof col === 'number') {
-            return col
-          }
-          return mysql2.escape(col)
-        })
-        .join(',')
-      return `(${rowStr}) \n`
-    })
-    return sql1 + cols + values
-  }
 }
---
+++ src/TableData.ts
@@ -0,0 +1,63 @@
+import mysql2 from 'mysql2/promise'
+import dayjs from 'dayjs'
+import { DBColumnTypes, isStringColumnType, TypesVal } from './DBFixtureJs'
+
+export type RowData = Array<string | number | Date | null>
+export type ColumnTypes = Array<{ columnName: string; columnType: TypesVal }>
+
+export class TableData {
+  readonly schemaName: string | null
+  readonly name: string
+  data: RowData[]
+  columnTypes: ColumnTypes
+  readonly emptyStr: string
+
+  get tableName() {
+    return this.schemaName ? `${this.schemaName}.${this.name}` : `${this.name}`
+  }
+
+  constructor(init: { schemaName?: string; name: string; data?: RowData[]; columnTypes: ColumnTypes; emptyStr: string }) {
+    this.schemaName = init.schemaName ?? null
+    this.name = init.name
+    this.data = init.data ?? []
+    this.columnTypes = init.columnTypes
+    this.emptyStr = init.emptyStr
+  }
+
+  public createInsertSql(): string {
+    const sql1 = `INSERT INTO ${this.tableName} `
+    const cols = '(' + this.columnTypes.map((v) => v.columnName).join(',') + ') VALUES \n'
+    const values = this.data.map((row) => {
+      const rowStr = row
+        .map((col, index) => {
+          const columnType = this.columnTypes[index].columnType
+          if (isStringColumnType(this.columnTypes[index].columnType)) {
+            if (col !== null && col.toString().match(this.emptyStr)) {
+              return mysql2.escape('')
+            }
+          }
+          if (columnType === DBColumnTypes.BIT) {
+            return `b'${col}'`
+          } else if (columnType === DBColumnTypes.DATETIME) {
+            const val = dayjs(col).format('YYYY-MM-DD HH:mm:ss')
+            return mysql2.escape(val)
+          } else if (columnType === DBColumnTypes.TIMESTAMP) {
+            const val = dayjs(col).format('YYYY-MM-DD HH:mm:ss')
+            return mysql2.escape(val)
+          } else if (columnType === DBColumnTypes.DATE) {
+            const val = dayjs(col).format('YYYY-MM-DD')
+            return mysql2.escape(val)
+          }
+          if (typeof col === 'string') {
+            return mysql2.escape(col)
+          } else if (typeof col === 'number') {
+            return col
+          }
+          return mysql2.escape(col)
+        })
+        .join(',')
+      return `(${rowStr}) \n`
+    })
+    return sql1 + cols + values
+  }
+}
